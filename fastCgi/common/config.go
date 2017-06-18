b4435a0e970a2a8860a20a6c4b29206d0cbc1022
qiangf <qiangf@qiangfdeMBP.lan>
2017-06-18 14:06:26 +0800

    增加json支持

--- fastCgi/common/config.go
+++ fastCgi/common/config.go
@@ -1,20 +1,39 @@
 package common
 
-//在本文件中配置基本的环境变量目录
-func getConfig() map[string]string {
-	var config = make(map[string]string)
-	//tcp监听的端口
-	config["port"] = ":4445"
-	//本机项目目录的src地址
-	config["go_src"] = "/Users/qiangf/go/src/"
-	//本机php bin命令下的php-cgi
-	config["php-cgi"] = "/usr/local/Cellar/php56/5.6.30_6.tmp/bin/php-cgi"
-	return config
-}
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/goless/config"
+)
 
 //只开放读的接口
 
 func GetRead(key string) string {
-	confMap := getConfig()
-	return confMap[key]
+
+	//获取json配置路径
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	file := dir + "/../common/config.json"
+	value := getJsonString(key, file)
+	return value
+}
+
+//读取file文件中的内容
+func getJsonString(name string, path string) string {
+	config := config.New(path)
+	res := config.Get(name)
+	ret := getName(res)
+	return ret
+}
+
+//把拿到的interface接口转换成string
+func getName(params ...interface{}) string {
+
+	strArray := make([]string, len(params))
+	for i, arg := range params {
+		strArray[i] = arg.(string)
+	}
+	aa := strings.Join(strArray, "_")
+	return aa
 }
